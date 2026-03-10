2013994cd7fcc84841fafc7b0379378f2bfb38b1
Ayush Patil <94798136+ayushpatil0810@users.noreply.github.com>
2026-03-10 18:51:31 +0530

    Add type inference and annotation examples in typesInTs.ts

---
+++ src/typesInTs.ts
@@ -0,0 +1,7 @@
+// type inference -> TS automatically know the type
+let car = "hyundai";
+let cups = Math.random() > 0.5 ? 10 : 5;
+
+//type annotation -> explicitly mentioning types
+let name: string = "Ayush";
+name = "MyName";
